93ca3078cc322f07a77e11d239c46f139624bd67
ycm23 <68059564+Yuchangminn1@users.noreply.github.com>
2024-09-04 21:22:16 +0900

    틀린부분 찾는중

--- Array0x03/Array/Array/StackNum2493.cpp
+++ Array0x03/Array/Array/StackNum2493.cpp
@@ -5,12 +5,13 @@
 using namespace std;
 namespace Num2493
 {
+	void POPSTACK(int& printNum, int i, std::stack<int>& stack_A);
 	void Solve1()
 	{
 		int printNum = 0;
 		int testNum = 0;
 		int currentNum = 0;
-
+		int topNum = 0;
 		stack<int> stack_A;
 
 		
@@ -19,81 +20,54 @@ namespace Num2493
 		for (int i = 0; i < testNum; i++) 
 		{
 			cin >> currentNum;
+			if (topNum < currentNum) 
+			{
+				topNum = currentNum;
+			}
 			if(!stack_A.empty())
 			{
-				if (stack_A.top() > currentNum)
-				{
+				if (topNum == currentNum)	//있던 숫자보다 더 큰 숫자 받아줄 탑이 없으므로
+											// ex 11 (6 7 8 9 10)  12
+				{							//printNum을 0으로 해야함
+					POPSTACK(printNum, i, stack_A);
+					printNum = 0;
+					cout << 0;
+					continue;
+				}
 
-					while (!stack_A.empty())
-					{
-						cout << printNum;
-						stack_A.pop();
-					}
+				if (stack_A.top() > currentNum) //들어온 값이 더 작을경우
+				{								// ex (6 7 8 9 10) 1
+												// 스택을 털기
+					POPSTACK(printNum, i, stack_A);
 					printNum = i;
 				}
 			} 
-			
+			if (topNum == currentNum) 
+			{
+				printNum = 0;
+				cout << 0;
+				continue;
+			}
 			stack_A.push(currentNum);
 
 		}
-		while (!stack_A.empty())
+		while (!stack_A.empty())				// 남으면 조건이 전에 들어온 수 보다 작다는거 
 		{
-			cout << printNum;
+			cout << testNum - stack_A.size();
 			stack_A.pop();
 		}
 
 		return;
 	}
-	/*void Solve1()
+	void POPSTACK(int& printNum, int i, std::stack<int>& stack_A)
 	{
-		stack<int> stack_A;
-		int testNum = 0;
-		cin >> testNum;
-		int _num = 0;
-		int cursor = 1000000;
-		int coutTop = 0;
-		while (testNum > 0) 
+		printNum = i - stack_A.size();
+		while (!stack_A.empty())
 		{
-			--testNum;
-			cin >> _num;
-			stack_A.push(_num);
-
-
-		}
-		while(!stack_A.empty())
-		{
-
-			if (cursor < stack_A.top())
-			{
-				cout << stack_A.size() << "\n";
-
-				coutTop--;
-
-				if (coutTop > 0)
-				{
-					while (coutTop > 0)
-					{
-						cout << stack_A.size() << "\n";
-						coutTop--;
-					}
-				}
-			}
-
-			cursor = stack_A.top();
-			coutTop++;
+			cout << printNum;
 			stack_A.pop();
-
 		}
-		while (coutTop > 0) 
-		{
-			cout << 0 << "\n";
-			coutTop--;
-		}
-
-		return;
-
-	}*/
-
+	}
 
 }
 
